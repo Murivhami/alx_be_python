5b2e02755ac747d070046493da514cc817e9d4da
Murivhami <tshedzamurivhami@gmail.com>
2024-12-15 10:42:39 -0800

    Python basic

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Defining variables
+number1 = 10
+number2 = 2
+
+#Arithmetic operations
+sum = number1 + number2
+difference = number1 - number2
+product = number1*number2
+
+#Outputs
+print("Addition of 10 and 5 is", sum)
+print("Subtraction of 10 and 5 is", difference)
+print("Product of 10 and 5 is", product)
+
