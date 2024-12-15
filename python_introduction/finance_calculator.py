3f5ac00c735bcab67dc1f3dbb89793df3f5cc375
Murivhami <tshedzamurivhami@gmail.com>
2024-12-15 13:19:54 -0800

    Finance calculator

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,10 +1,10 @@
 #!/bin/bash
-# The objective of the task is to
+# The objective of the task is to 
 # calculate the user’s monthly savings based on inputted monthly income and expenses.
 
 # User Input for Financial Details:
 monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expenses = int(input("Enter your total monthly expenses:"))
+total_monthly_expenses = int(input("Enter your total monthly expenses: "))
 
 #Calculate monthly savings
 monthly_savings = monthly_income - total_monthly_expenses
@@ -14,5 +14,5 @@ simple_annual_interest_rate = 0.05
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
 #Output results
-print(f"Your monthly savings are $:{monthly_savings}.")
-print(f"Projected savings after one year, with interest, is: ${int(projected_savings)}")
+print(f"Your monthly savings are ${monthly_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings:.2f}")
