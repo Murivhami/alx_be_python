ef3876ba2895c9d9a3aefcffcbe95ad7782ca3d8
Murivhami <tshedzamurivhami@gmail.com>
2024-12-15 11:14:14 -0800

    Simple interest calculation

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,11 @@
+#!/bin/bash
+#Calculation of simple interest
+
+#Defining variables
+principal = 1000
+rate = 0.05
+time = 3
+
+#Calculating interes
+interest = principal*rate*time
+print(f"The simple interest is: {interest} ")
