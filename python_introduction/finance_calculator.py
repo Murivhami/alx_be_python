069d0e47f4b40f1e1e4e86c73c463abc26fcab1d
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2024-12-16 08:11:21 +0200

    Update finance_calculator.py

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -3,10 +3,10 @@
 
 # User Input for Financial Details:
 monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expenses = int(input("Enter your total monthly expenses: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
 #Calculate monthly savings
-monthly_savings = float(monthly_income) - float(total_monthly_expenses)
+monthly_savings = float(monthly_income) - float(monthly_expenses)
 
 #Project Annual Savings
 #simple_annual_interest_rate = 0.05
