caa14f680af5a8395e175872b8f7f59f0a175086
Murivhami <tshedzamurivhami@gmail.com>
2024-12-21 12:26:24 -0800

    Multiple

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -3,7 +3,7 @@
 number = int(input("Enter a number to see its multiplication table: "))
 
 
-i = 0
-for i in (1,2,3,4,5,6,7,8,9,10):
-            product = number * i
-            print(f"{number} * {i} = {product}" )
+Y = 0
+for Y in (1,2,3,4,5,6,7,8,9,10):
+            product = number * Y
+            print(f"{number} * {Y} = {Z}" )
