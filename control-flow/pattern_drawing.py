ec5175b1ff985f2d6c8a0150219bd09b8e5f9916
Murivhami <tshedzamurivhami@gmail.com>
2024-12-22 08:55:49 -0800

    Added4

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,13 +1,12 @@
 #!/bin/bash
-positive_integer = int(input("Enter a number"))
+positive_integer = int(input("Enter the size of the pattern: "))
 
 i = 0
-
 
 while i < positive_integer:
     j = 0
     while j < positive_integer:
-        print("*", end ="")
+        print("*", end="")
         j += 1
     print()
     i += 1
