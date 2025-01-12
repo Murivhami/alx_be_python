574abb84d0eacdcde813f80ac2fda1f3440e62a3
Murivhami <tshedzamurivhami@gmail.com>
2025-01-12 10:50:47 -0800

    Ex4

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,30 @@
+#!/bin/bash
+#Define functions to convert temperatures between Celsius and Fahrenheit.
+
+#1. Define Global Conversion Factors:
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
+
+
+#2. Implement Conversion Functions:
+
+def convert_to_celsius(fahrenheit):
+    return(fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
+def convert_to_fahrenheit(celsius):
+    return(celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
+
+def main():
+    temperature = float(input("Enter the temperature to convert: "))
+    unit = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+    if unit == "C":
+         converted = convert_to_fahrenheit(temperature)
+         print(f"{temperature}°C is {converted}°F")
+    elif unit == "F":
+        converted = convert_to_celsius(temperature)
+        print(f"{temperature}°F is {converted}°C")
+    else:
+        print("Invalid option")
+
+if __name__ == "_main_":
+    main()
