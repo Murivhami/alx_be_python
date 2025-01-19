6e40ac50d31dd4295915c6a1f1a9301baf8be38c
Murivhami <tshedzamurivhami@gmail.com>
2025-01-18 23:26:24 -0800

    W6

---
+++ programming_paradigm/robust_division_calculator.py
@@ -0,0 +1,20 @@
+#!/bin/bash
+#Definiton of a function.
+
+def safe_divide(numerator, denominator):
+    try:
+        numerator = float(input("Numerator: "))
+        denominator = float(input("Denominator: "))
+        result = numerator/denominator
+        if denominator == 0:
+            raise ZeroDivisionError
+    except ZeroDivisionError:
+        return("Error: Cannot divide by zero.")
+    except ValueError:
+        return("Error: Please enter numeric values only.")
+    else:
+        return(f"The result of the division is {result:.2f}")
+
+
+output = safe_divide(numerator=2, denominator=1)
+print(f"{output}")
