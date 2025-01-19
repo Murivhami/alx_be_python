75bde46b4c2e6e737ee8917f23d10fd9c92070a0
Murivhami <tshedzamurivhami@gmail.com>
2025-01-18 23:33:25 -0800

    W6

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -13,7 +13,7 @@ def safe_divide(numerator, denominator):
     except ValueError:
         return("Error: Please enter numeric values only.")
     else:
-        return(f"The result of the division is {result:.2f}")
+        return(f"The result of the division is {result:.1f}")
 
 
 output = safe_divide(numerator=2, denominator=1)
