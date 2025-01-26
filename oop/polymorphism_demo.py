a6a369938849fe3e69f2b913ab8234638202306d
Tshedza Confidence <99668617+Murivhami@users.noreply.github.com>
2025-01-26 19:22:11 +0200

    Update polymorphism_demo.py

--- oop/polymorphism_demo.py
+++ oop/polymorphism_demo.py
@@ -4,17 +4,21 @@ import math
 class Shape:
     def area(self):
         raise NotImplementedError
-
+    
 class Rectangle(Shape):
     def __init__(self, length, width):
         super().__init__()
-        self.area = length * width
+        self.length = length
+        self.width = width
+        self.area = self.length * self.width
     def __str__(self):
         return f"The area of the Rectangle is: {self.area}"
-
+    
 class Circle(Shape):
     def __init__(self, radius):
         super().__init__()
-        area = math.pi * (radius)^2
+        self.radius = radius
+        area = math.pi * self.radius, "** 2"
+    
     def __str__(self):
         return f"The area of the Circle is: {self.area}"
